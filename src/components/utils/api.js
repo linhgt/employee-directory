b6259a707c2bb753e58442211414ed83869ecaa7
Linh Nguyen <linhnhatmen@gmail.com>
2021-02-17 08:36:24 +0700

    Updates

--- src/components/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASEURL = "https://randomuser.me/api/?results=100";
-//Random user generator api
-function search(){
-    return axios.get(BASEURL);
-}
-
-export default search;
